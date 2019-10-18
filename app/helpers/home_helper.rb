fb2388ff31b9fbbf5a664c0aabcacd158f24babd
Pushkar Chitale <chitalepushkar@gmail.com>
2019-10-18 19:15:25 +0530

    Handling active nav issue

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,4 +4,8 @@ module HomeHelper
     return File.read(file_path).html_safe if File.exists?(file_path)
     '(not found)'
   end
+
+ def active_class(link_path)
+  current_page?(action: link_path) ? "active" : ""
+ end
 end
